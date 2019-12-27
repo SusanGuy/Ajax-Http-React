f3c28ee1af0d2110f9e9bbb6ae318f465b5da5c8
Susan Subedi <susansubedi34@gmail.com>
2019-12-26 19:32:10 -0600

    Get req in different lifecycle method

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -1,22 +1,64 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import axios from "axios";
 
-import './FullPost.css';
+import "./FullPost.css";
 
 class FullPost extends Component {
-    render () {
-        let post = <p>Please select a Post!</p>;
-        post = (
-            <div className="FullPost">
-                <h1>Title</h1>
-                <p>Content</p>
-                <div className="Edit">
-                    <button className="Delete">Delete</button>
-                </div>
-            </div>
-
+  state = {
+    loadedContent: null
+  };
+  async componentDidUpdate() {
+    if (this.props.id) {
+      if (
+        !this.state.loadedContent ||
+        this.state.loadedContent.id !== this.props.id
+      ) {
+        const post = await axios.get(
+          "https://jsonplaceholder.typicode.com/posts/" + this.props.id
         );
-        return post;
+
+        this.setState({
+          loadedContent: post.data
+        });
+      }
     }
+  }
+
+  render() {
+    let post = (
+      <p
+        style={{
+          textAlign: "center"
+        }}
+      >
+        Please select a Post!
+      </p>
+    );
+    if (this.props.id) {
+      post = (
+        <p
+          style={{
+            textAlign: "center"
+          }}
+        >
+          Loading...
+        </p>
+      );
+    }
+
+    if (this.state.loadedContent) {
+      post = (
+        <div className="FullPost">
+          <h1>{this.state.loadedContent.title}</h1>
+          <p>{this.state.loadedContent.body}</p>
+          <div className="Edit">
+            <button className="Delete">Delete</button>
+          </div>
+        </div>
+      );
+    }
+    return post;
+  }
 }
 
 export default FullPost;
--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,28 +1,60 @@
-import React, { Component } from 'react';
-
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
-import './Blog.css';
+import React, { Component } from "react";
+import axios from "axios";
+import Post from "../../components/Post/Post";
+import FullPost from "../../components/FullPost/FullPost";
+import NewPost from "../../components/NewPost/NewPost";
+import "./Blog.css";
 
 class Blog extends Component {
-    render () {
-        return (
-            <div>
-                <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
-                </section>
-                <section>
-                    <FullPost />
-                </section>
-                <section>
-                    <NewPost />
-                </section>
-            </div>
-        );
-    }
+  state = {
+    posts: [],
+    selectedPostId: null
+  };
+  async componentDidMount() {
+    const posts = await axios.get("https://jsonplaceholder.typicode.com/posts");
+    const data = posts.data.slice(0, 4);
+
+    const updatedPosts = data.map(post => {
+      return {
+        ...post,
+        author: "Max"
+      };
+    });
+
+    this.setState({
+      posts: updatedPosts
+    });
+  }
+
+  postsSelectedHandler = id => {
+    this.setState({
+      selectedPostId: id
+    });
+  };
+
+  render() {
+    const posts = this.state.posts.map(post => {
+      return (
+        <Post
+          author={post.author}
+          title={post.title}
+          key={post.id}
+          clicked={() => this.postsSelectedHandler(post.id)}
+        />
+      );
+    });
+    return (
+      <div>
+        <section className="Posts">{posts}</section>
+        <section>
+          <FullPost id={this.state.selectedPostId} />
+        </section>
+        <section>
+          <NewPost />
+        </section>
+      </div>
+    );
+  }
 }
 
 export default Blog;
